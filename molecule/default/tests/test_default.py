580783b79b969aef7d307dc73aecc57e8d29a18f
Fabrizio Monti <fabrizio.monti@welaika.com>
2019-07-29 14:30:33 +0200

    Copy pasted tasks in here

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_vim_package_exist(host):
+    assert host.package('vim').is_installed
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_symlink_created(host):
+    f = host.file('/home/jerry/.vimrc')
+    assert f.linked_to == '/home/jerry/.vim/vimrc'
